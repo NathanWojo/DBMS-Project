968cc327ac97193b77cbce2f874f7e51cca9cf05
Stepty <stephen7ni@gmail.com>
2025-04-18 15:25:24 -0500

    add fade in transition & home svg

--- plazaReport.php
+++ plazaReport.php
@@ -8,7 +8,11 @@
 </head>
 <body>
   <header>
-    <a href="home.html" class="home-button">Home</a>
+    <a href="home.html" class="home-button">
+        <svg width="20px" height="20px" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1 6V15H6V11C6 9.89543 6.89543 9 8 9C9.10457 9 10 9.89543 10 11V15H15V6L8 0L1 6Z" fill="#FFFFFF"/>
+        </svg>
+    </a>
     <h1>Plaza Report</h1>
   </header>
 
