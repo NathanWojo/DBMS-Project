6255d8b96acfd6cda4c7bc82e9758b36f7670b76
NathanWojo <wojtowicz.nathan@outlook.com>
2025-04-17 19:44:27 -0500

    combine files

--- project.py
+++ project.py
@@ -1,10 +1,34 @@
-import sys
-import traceback
-import logging
-import python_db
+import mysql.connector
+from tabulate import tabulate
 
-mysql_username = 'nawojtow'
-mysql_password = 'Iox4quai'
+def open_database(hostname, user_name, mysql_pw, database_name):
+    global conn
+    conn = mysql.connector.connect(host=hostname,
+                                   user=user_name,
+                                   password=mysql_pw,
+                                   database=database_name
+                                   )
+    global cursor
+    cursor = conn.cursor()
 
-#will make calls to python_db depending on what project.php sends
-#to actually do things in mysql
+def printFormat(result):
+    header = []
+    for cd in cursor.description:  # get headers
+        header.append(cd[0])
+    print('')
+    print('Query Result:')
+    print('')
+    return(tabulate(result, headers=header))  # print results in table format
+
+def executeSelect(query):
+    cursor.execute(query)
+    res = printFormat(cursor.fetchall())
+    return res
+
+def executeUpdate(query):  # use this function for delete and update
+    cursor.execute(query)
+    conn.commit()
+
+def close_db():  # use this function to close db
+    cursor.close()
+    conn.close()
